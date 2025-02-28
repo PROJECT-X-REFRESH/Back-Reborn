dbf7dacc3285854688a5b94a413ef8d7278ce414
MinJunKKang <kmg4613@naver.com>
2025-02-28 20:43:27 +0900

    Intellij 최적화

--- src/main/java/com/reborn/back/board/repository/BoardLikeRepository.java
+++ src/main/java/com/reborn/back/board/repository/BoardLikeRepository.java
@@ -3,7 +3,6 @@ package com.reborn.back.board.repository;
 import com.reborn.back.domain.board.Board;
 import com.reborn.back.domain.board.BoardLike;
 import com.reborn.back.domain.user.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
