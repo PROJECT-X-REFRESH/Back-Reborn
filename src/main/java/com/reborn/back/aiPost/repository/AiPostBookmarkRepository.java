b65d66c6ec301b138be920e345506233c485fbcb
MinJunKKang <kmg4613@naver.com>
2025-03-07 10:57:41 +0900

    필요없는 패키지 제거

--- src/main/java/com/reborn/back/aiPost/repository/AiPostBookmarkRepository.java
+++ src/main/java/com/reborn/back/aiPost/repository/AiPostBookmarkRepository.java
@@ -1,10 +1,8 @@
 package com.reborn.back.aiPost.repository;
 
-import com.reborn.back.domain.board.BoardBookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AiPostBookmarkRepository extends JpaRepository<BoardBookmark, Long> {
+public interface AiPostBookmarkRepository {
 
 }
