471e502574921e2eb6397ec6c48854b98a806a63
MinJunKKang <kmg4613@naver.com>
2025-03-07 15:01:08 +0900

    likeCount 제거 및 Bookmark 제거

--- src/main/java/com/reborn/back/domain/board/Board.java
+++ src/main/java/com/reborn/back/domain/board/Board.java
@@ -6,7 +6,6 @@ import com.reborn.back.domain.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,11 +34,11 @@ public class Board extends BaseEntity {
     @Column(name = "bAttachImg", length = 255)
     private String attachImg;
 
-    @Column(name = "bLikeCount", nullable = false)
-    private Integer likeCount;
-
     @Column(name = "bCommentCount", nullable = false)
     private Integer commentCount;
+
+    @Column(name = "bviewCount", nullable = false)
+    private Integer viewCount;
 
     // FK: uid → User(uid)
     @ManyToOne(fetch = FetchType.LAZY)
@@ -47,15 +46,6 @@ public class Board extends BaseEntity {
     private User user;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BoardView> boardViewList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardLike> boardLikeList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BoardBookmark> boardBookmarkList = new ArrayList<>();
-
-    public void updateLikeCount(Integer likeCount) { this.likeCount = likeCount; }
-
-    public void updateCommentCount(Integer commentCount) { this.commentCount = commentCount; }
 }
