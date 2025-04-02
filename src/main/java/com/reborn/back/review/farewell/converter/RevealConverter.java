20abcb470a380fd1e6e890db2f3f438b48686f32
MinJunKKang <kmg4613@naver.com>
2025-04-02 19:21:34 +0900

    Reveal res, req Dto 설정 생성, 컨텐츠 Boolean 변경, 글 작성 및 step 증가 서비스 Converter, Repsitory

---
+++ src/main/java/com/reborn/back/review/farewell/converter/RevealConverter.java
@@ -0,0 +1,37 @@
+package com.reborn.back.review.farewell.converter;
+
+import com.reborn.back.domain.review.farewell.Farewell;
+import com.reborn.back.domain.review.farewell.Reveal;
+import com.reborn.back.review.farewell.dto.RevealResponseDto;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class RevealConverter {
+    public static Reveal saveReveal(Farewell farewell) {
+        return Reveal.builder()
+                .feed(false)
+                .walk(false)
+                .snack(false)
+                .farewell(farewell)
+                .build();
+    }
+
+    public static RevealResponseDto.DetailRevealDto toDto(Reveal reveal) {
+        return RevealResponseDto.DetailRevealDto.builder()
+                .feed(reveal.getFeed())
+                .snack(reveal.getSnack())
+                .walk(reveal.getWalk())
+                .emotionState(String.valueOf(reveal.getEmotion().getState()))
+                .build();
+    }
+
+    public static RevealResponseDto.ReviewRevealDto toReviewDto(Reveal reveal) {
+        return RevealResponseDto.ReviewRevealDto.builder()
+                .createdAt(reveal.getCreatedAt())
+                .contents(reveal.getContents())
+                .pos(reveal.getEmotion().getPos())
+                .neg(reveal.getEmotion().getNeg())
+                .emotionState(reveal.getEmotion().getState())
+                .build();
+    }
+}
