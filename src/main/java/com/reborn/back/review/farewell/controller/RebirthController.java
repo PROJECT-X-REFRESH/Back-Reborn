7cfe55facde814c2fcfa2f6ce51c17d5fa185e9f
MinJunKKang <kmg4613@naver.com>
2025-04-02 19:27:12 +0900

    Rebirth res, req Dto 설정 생성, 컨텐츠 Boolean 변경(리본 설정, 로직상 에러가 났던 것으로 기억해서 이 부분 수정 필요) Converter, Repsitory, Controller 코드 구현

--- src/main/java/com/reborn/back/review/farewell/controller/RebirthController.java
+++ src/main/java/com/reborn/back/review/farewell/controller/RebirthController.java
@@ -1,13 +1,91 @@
 package com.reborn.back.review.farewell.controller;
 
+import com.reborn.back.domain.review.farewell.Rebirth;
+import com.reborn.back.domain.user.User;
+import com.reborn.back.global.api.ApiResponse;
+import com.reborn.back.global.api.SuccessCode;
+import com.reborn.back.login.auth.mapper.CustomUserDetails;
+import com.reborn.back.login.service.UserService;
+import com.reborn.back.review.farewell.dto.RebirthRequestDto;
+import com.reborn.back.review.farewell.dto.RebirthResponseDto.DetailRebirthDto;
+import com.reborn.back.review.farewell.service.RebirthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "", description = "")
+@Tag(name = "rebirth", description = "rebirth 관련 API")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/farewell/{farewellId}/rebirth")
 @RequiredArgsConstructor
 public class RebirthController {
+
+    private final RebirthService rebirthService;
+    private final UserService userService;
+
+    // 반려동물과 건강한 작별하기
+    @Operation(summary = "Rebirth 생성", description = "fstep에 따라 Rebirth을 생성하는 API")
+    @ApiResponses(value =  {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "Rebirth_2011", description = "반려동물과 건강한 작별하기 생성이 완료되었습니다.")
+    })
+    @PostMapping
+    public ApiResponse<Integer> createRebirth(
+            @PathVariable Integer farewellId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+
+        Rebirth rebirth = rebirthService.createRebirth(farewellId);
+        return ApiResponse.onSuccess(SuccessCode.REBIRTH_CREATED, rebirth.getId());
+    }
+
+    @Operation(summary = "컨텐츠 상태 변경", description = "Rebirth 컨텐츠(wash, dress, ribbon{string})의 상태를 변경하는 API")
+    @ApiResponses(value =  {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REBIRTH_2003", description = "컨텐츠가 완료되었습니다.")
+    })
+    @PatchMapping("/{activityType}")
+    public ApiResponse<String> updateRebirthActivity(
+            @PathVariable Integer farewellId,
+            @PathVariable String activityType,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+
+        rebirthService.updateRebirthActivity(farewellId, activityType);
+        return ApiResponse.onSuccess(SuccessCode.REBIRTH_ACTIVITY_UPDATED, activityType);
+    }
+
+    @Operation(summary = "반려동물 편지 저장", description = "반려동물 편지 저장하는 메서드입니다.")
+    @ApiResponses(value =  {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REBIRTH_2003", description = "편지 저장이 완료되었습니다.")
+    })
+    @PostMapping("/write")
+    public ApiResponse<Boolean> write(
+            @PathVariable Integer farewellId,
+            @RequestBody RebirthRequestDto rebirthRequestDto,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+
+        rebirthService.writeRebirth(farewellId, rebirthRequestDto);
+
+        return ApiResponse.onSuccess(SuccessCode.REBIRTH_WRITE_COMPLETED, true);
+    }
+
+    @Operation(summary = "Rebirth 진행 상황", description = "진행중인 Rebirth의 상태를 전달하는 API")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REBIRTH_2002", description = "나의 감정 드러내기 조회가 완료되었습니다.")
+    })
+    @GetMapping("/view")
+    public ApiResponse<DetailRebirthDto> getDetailRebirth(
+            @PathVariable Integer farewellId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+
+        DetailRebirthDto detail = rebirthService.getDetailRebirth(farewellId);
+        return ApiResponse.onSuccess(SuccessCode.REBIRTH_DETAIL_VIEW_SUCCESS, detail);
+    }
 }
--- src/main/java/com/reborn/back/review/farewell/service/RebirthService.java
+++ src/main/java/com/reborn/back/review/farewell/service/RebirthService.java
@@ -1,5 +1,15 @@
 package com.reborn.back.review.farewell.service;
 
+import com.reborn.back.domain.review.farewell.Farewell;
+import com.reborn.back.domain.review.farewell.Rebirth;
+import com.reborn.back.global.api.ErrorCode;
+import com.reborn.back.global.exception.GeneralException;
+import com.reborn.back.review.farewell.converter.RebirthConverter;
+import com.reborn.back.review.farewell.dto.RebirthRequestDto;
+import com.reborn.back.review.farewell.dto.RebirthResponseDto.DetailRebirthDto;
+import com.reborn.back.review.farewell.repository.FarewellRepository;
+import com.reborn.back.review.farewell.repository.RebirthRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +18,73 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RebirthService {
+    private final RebirthRepository rebirthRepository;
+    private final FarewellRepository farewellRepository;
+
+    @Transactional
+    public Rebirth createRebirth(Integer farewellId) {
+        Farewell farewell = farewellRepository.findById(farewellId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.FAREWELL_NOT_FOUND));
+
+        Rebirth rebirth = RebirthConverter.saveRebirth(farewell);
+        rebirthRepository.save(rebirth);
+
+        farewell.setStep(farewell.getStep() + 1);
+
+        return rebirth;
+    }
+
+    @Transactional
+    public void updateRebirthActivity(Integer farewellId, String activityType) {
+        Farewell farewell = farewellRepository.findById(farewellId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.FAREWELL_NOT_FOUND));
+
+        Rebirth rebirth = rebirthRepository.findTopByFarewellOrderByCreatedAtDesc(farewell)
+                .orElseThrow(() -> new GeneralException(ErrorCode.REBIRTH_NOT_FOUND));
+
+        // activityType에 따라 적절한 필드를 true로 변경
+        switch (activityType.toLowerCase()) {
+            case "wash":
+                rebirth.setWash(true);
+                break;
+            case "dress":
+                rebirth.setDress(true);
+                break;
+            case "Yribbon":
+                rebirth.setRibbon("YELLOW");
+                break;
+            case "Bribbon":
+                rebirth.setRibbon("BLACK");
+                break;
+            default:
+                throw new GeneralException(ErrorCode.INVALID_ACTIVITY_TYPE);
+        }
+
+        rebirthRepository.save(rebirth);
+    }
+
+    @Transactional
+    public Rebirth writeRebirth(Integer farewellId, RebirthRequestDto rebirthRequestDto) {
+        Farewell farewell = farewellRepository.findById(farewellId)
+                .orElseThrow(() -> GeneralException.of(ErrorCode.FAREWELL_NOT_FOUND));
+
+        Rebirth rebirth = rebirthRepository.findTopByFarewellOrderByCreatedAtDesc(farewell)
+                .orElseThrow(() -> GeneralException.of(ErrorCode.REBIRTH_NOT_FOUND));
+
+        rebirth.setPetPost(rebirth.getPetPost());
+
+        return rebirth;
+    }
+
+    public DetailRebirthDto getDetailRebirth(Integer farewellId) {
+        Farewell farewell = farewellRepository.findById(farewellId)
+                .orElseThrow(() -> GeneralException.of(ErrorCode.FAREWELL_NOT_FOUND));
+
+        Rebirth rebirth = rebirthRepository.findTopByFarewellOrderByCreatedAtDesc(farewell)
+                .orElseThrow(() -> GeneralException.of(ErrorCode.REBIRTH_NOT_FOUND));
+
+        return RebirthConverter.toDto(rebirth);
+    }
+
+
 }
