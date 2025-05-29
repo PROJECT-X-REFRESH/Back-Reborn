1177f02d3615d25aad5071fa73d09549fea778fd
MinJunKKang <kmg4613@naver.com>
2025-05-30 05:37:19 +0900

    grade 없을 경우 null 전달

--- src/main/java/com/reborn/back/review/farewell/dto/CounselingCenterDto.java
+++ src/main/java/com/reborn/back/review/farewell/dto/CounselingCenterDto.java
@@ -7,13 +7,24 @@ import lombok.*;
 @Getter @Builder @NoArgsConstructor @AllArgsConstructor
 public class CounselingCenterDto {
 
-    @Schema(description = "상담소 이름")        private String displayName;
-    @Schema(description = "상담소 주소")        private String formattedAddress;
-    @Schema(description = "국가 전화번호")      private String nationalPhoneNumber;
-    @Schema(description = "위도")              private Double latitude;
-    @Schema(description = "경도")              private Double longitude;
+    @Schema(description = "상담소 이름")
+    private String displayName;
 
-    @Schema(description = "ykiho (없으면 null)") private String ykiho;
-    @Schema(description = "asmGrd09 (없으면 \"평가정보가 없습니다\")")
+    @Schema(description = "상담소 주소")
+    private String formattedAddress;
+
+    @Schema(description = "국가 전화번호")
+    private String nationalPhoneNumber;
+
+    @Schema(description = "위도")
+    private Double latitude;
+
+    @Schema(description = "경도")
+    private Double longitude;
+
+    @Schema(description = "ykiho (없으면 null)")
+    private String ykiho;
+
+    @Schema(description = "asmGrd09 (없으면 null)")
     private String grade;
 }
