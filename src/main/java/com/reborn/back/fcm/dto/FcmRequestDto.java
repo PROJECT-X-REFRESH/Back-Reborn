15d4b0ad77bdbd94c8b5ab1b2480f999f9e63859
MinJunKKang <kmg4613@naver.com>
2025-06-04 16:05:46 +0900

    댓글 작성 시에 보드 id 전달

--- src/main/java/com/reborn/back/fcm/dto/FcmRequestDto.java
+++ src/main/java/com/reborn/back/fcm/dto/FcmRequestDto.java
@@ -1,22 +1,19 @@
 package com.reborn.back.fcm.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FcmRequestDto {
-    private String token;
-
-    private String title;
-
-    private String body;
-
-    @Builder(toBuilder = true)
-    public FcmRequestDto(String token, String title, String body) {
-        this.token = token;
-        this.title = title;
-        this.body = body;
-    }
+    private String token;                 // 단말 토큰
+    private String title;                 // 푸시 제목
+    private String body;                  // 푸시 내용
+    private Map<String, String> data;     // 추가 데이터 (boardId 등)
 }
